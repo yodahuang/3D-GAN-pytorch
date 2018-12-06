f213f0a9120c775e56bb4bbf1befb87e9f515e18
Yanda Huang <ward11sdo@qq.com>
2018-12-06 18:53:48 -0500

    Fake SVM

--- evaluate.py
+++ evaluate.py
@@ -1,23 +1,20 @@
 import click
 import numpy as np
-import torch
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 
 from dataset import ShapeNet, config
-from nets import Discriminator
-
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def get_category_data(category_datasets, category_labels, discriminator, is_train):
+def get_category_data(category_datasets, category_labels, is_train):
     representations = []
     labels = []
     for dataset, label in zip(category_datasets, category_labels):
         for real_X in tqdm(dataset.gen(is_train)):
-            real_X = real_X.to(DEVICE)
-            representations.append(discriminator.forward_eval(real_X).detach().cpu().numpy())
+            batch_size = real_X.size(0)
+            real_X = real_X.view(batch_size, -1).numpy()
+            representations.append(real_X)
             labels.append(np.ones(real_X.shape[0]) * label)
     representations = np.vstack(representations)
     labels = np.hstack(labels)
@@ -25,12 +22,8 @@ def get_category_data(category_datasets, category_labels, discriminator, is_trai
 
 
 @click.command()
-@click.option('-m', '--model_path', required=True, help='Discriminator model path')
 @click.option('-b', '--batch_size', type=int, default=64)
-def evaluate_single(model_path, batch_size):
-    discriminator = Discriminator().to(DEVICE)
-    discriminator.eval()
-    discriminator.load_state_dict(torch.load(model_path))
+def evaluate_single(batch_size):
 
     # The idea of config is just pure stupid
     config.set_batchsize(batch_size)
@@ -39,13 +32,13 @@ def evaluate_single(model_path, batch_size):
     category_labels = range(len(all_category_names))
     category_datasets = [ShapeNet([c], config, infinity=False) for c in all_category_names]
 
-    training_representations, training_labels = get_category_data(category_datasets, category_labels, discriminator, True)
+    training_representations, training_labels = get_category_data(category_datasets, category_labels, True)
 
     # Classifier
-    clf = svm.LinearSVC(penalty='l2', C=0.01, class_weight='balanced')
+    clf = svm.LinearSVC(penalty='l2', C=0.01, class_weight='balanced', verbose=1)
     clf.fit(training_representations, training_labels)
 
-    test_representations, test_labels = get_category_data(category_datasets, category_labels, discriminator, False)
+    test_representations, test_labels = get_category_data(category_datasets, category_labels, False)
 
     predictions = clf.predict(test_representations)
     print(accuracy_score(test_labels, predictions))
